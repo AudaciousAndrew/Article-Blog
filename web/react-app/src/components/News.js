a18004e8292d9aadcc99517931df2b1cf6ea39b4
psemenov <sp17september@gmail.com>
2018-02-03 23:10:52 +0300

    test

---
+++ web/react-app/src/components/News.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Menu from './Menu';
+
+class News extends Component{
+
+  render(){
+    console.log("lol");
+    return(
+      <div>
+        <Menu />
+        there will be some news
+      </div>
+    )
+  }
+}
+
+export default News;

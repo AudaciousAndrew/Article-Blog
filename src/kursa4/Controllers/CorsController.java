1665121df8598cbd46d5442a978a378a5bd48290
AudaciousAndrew <pukamirka@gmail.com>
2018-02-04 21:19:30 +0300

    try fix corsfilters3

---
+++ src/kursa4/Controllers/CorsController.java
@@ -0,0 +1,27 @@
+package kursa4.Controllers;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+
+public class CorsController {
+    @GET
+    @Path("/")
+    @Produces({MediaType.TEXT_PLAIN})
+    public Response index() {
+        return Response
+                .status(200)
+                .header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Credentials", "true")
+                .header("Access-Control-Allow-Headers",
+                        "origin, content-type, accept, authorization")
+                .header("Access-Control-Allow-Methods",
+                        "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+                .entity("")
+                .build();
+    }
+
+}
--- src/kursa4/Filters/CorsFilter.java
+++ src/kursa4/Filters/CorsFilter.java
@@ -11,9 +11,9 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
 import java.io.IOException;
 
-
-@Provider
-public class CorsFilter  implements javax.ws.rs.container.ContainerResponseFilter{
+//
+//@Provider
+//public class CorsFilter  implements javax.ws.rs.container.ContainerResponseFilter{
 //    @Override
 //    public ContainerResponse filter(ContainerRequest req, ContainerResponse contResp) {
 //
@@ -30,19 +30,19 @@ public class CorsFilter  implements javax.ws.rs.container.ContainerResponseFilte
 //        contResp.setResponse(resp.build());
 //        return contResp;
 //    }
-
-    @Override
-    public void filter(ContainerRequestContext requestContext,
-                       ContainerResponseContext responseContext) throws IOException {
-        responseContext.getHeaders().add(
-                "Access-Control-Allow-Origin", "*");
-        responseContext.getHeaders().add(
-                "Access-Control-Allow-Credentials", "true");
-        responseContext.getHeaders().add(
-                "Access-Control-Allow-Headers",
-                "origin, content-type, accept, authorization");
-        responseContext.getHeaders().add(
-                "Access-Control-Allow-Methods",
-                "GET, POST, PUT, DELETE, OPTIONS, HEAD");
-    }
-}
+//
+//    @Override
+//    public void filter(ContainerRequestContext requestContext,
+//                       ContainerResponseContext responseContext) throws IOException {
+//        responseContext.getHeaders().add(
+//                "Access-Control-Allow-Origin", "*");
+//        responseContext.getHeaders().add(
+//                "Access-Control-Allow-Credentials", "true");
+//        responseContext.getHeaders().add(
+//                "Access-Control-Allow-Headers",
+//                "origin, content-type, accept, authorization");
+//        responseContext.getHeaders().add(
+//                "Access-Control-Allow-Methods",
+//                "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+//    }
+//}
